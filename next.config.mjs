a7e7994a9251aa89902eebdc89ccdaf735cdbf56
InSeong Lee <randy80@gmail.com>
2025-08-17 22:53:28 +0900

    프로젝트 초기 설정: .gitignore 추가, Nextra 및 MDX 구성 요소 설정, Next.js 구성 파일 작성, GitHub Pages 배포 워크플로우 추가, 기본 페이지 및 콘텐츠 파일 생성

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  search: false,
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: 'https://randy80.github.io/', // GitHub 저장소 이름으로 변경
+  assetPrefix: 'https://randy80.github.io/', // GitHub 저장소 이름으로 변경
+})

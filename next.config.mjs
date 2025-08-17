6ac2e67b02d1616707fce7b18a04f0b7c78af223
InSeong Lee <randy80@gmail.com>
2025-08-17 23:01:03 +0900

    Next.js 구성 파일에서 basePath와 assetPrefix를 GitHub 저장소 URL에서 루트로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,6 @@ export default withNextra({
   images: {
     unoptimized: true
   },
-  basePath: 'https://randy80.github.io/', // GitHub 저장소 이름으로 변경
-  assetPrefix: 'https://randy80.github.io/', // GitHub 저장소 이름으로 변경
+  basePath: '/', // GitHub 저장소 이름으로 변경
+  assetPrefix: '/', // GitHub 저장소 이름으로 변경
 })
